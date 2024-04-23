1b9b7844216d54859b4ce4084e6a3ccc78f668c4
styxxx <145310090+itsStyxxxx@users.noreply.github.com>
2024-04-23 11:47:02 +0800

    Add files via upload

---
+++ bimandana/perulangan2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perulangan 2</title>
+</head>
+<body>
+    <?php
+        $bil = 1;
+        while ($bil <= 17)
+        {
+        echo "Bilangan sekarang adalah $bil<br />";
+        $bil++;
+        }
+        echo "Nilai bilangan selanjutnya adalah:".$bil;
+        ?>
+</body>
+</html>
